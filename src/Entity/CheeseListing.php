4806c31f8fe1f1ed18ef693ca1892764131dd0d0
yykoca <yykoca.08@gmail.com>
2022-12-15 22:04:19 +0100

    adding normalization-, denormalizationContext

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -7,9 +7,22 @@ use ApiPlatform\Metadata\ApiResource;
 use App\Repository\CheeseListingRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Carbon\Carbon;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: CheeseListingRepository::class)]
-#[ApiResource(shortName: "cheeses")]
+#[ApiResource(
+    // reading data --> normalization
+    normalizationContext: [
+        'groups' => ['cheese_listing:read'], 
+        'swagger_definition_name' => 'Read'
+    ], // ['groups' => ['[GROUP_NAME]:read']]
+    // writing data --> normalization
+    denormalizationContext: [
+        'groups' => ['cheese_listing:write'],
+        'swagger_definition_name' => 'Write'
+    ],
+    shortName: "cheeses"
+)]
 class CheeseListing
 {
     #[ORM\Id]
@@ -18,19 +31,22 @@ class CheeseListing
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['cheese_listing:read', 'cheese_listing:write'])] // #[Groups(['[GROUP_NAME]:read'])]
     private ?string $title = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['cheese_listing:read'])]
     private ?string $description = null;
 
     #[ORM\Column]
+    #[Groups(['cheese_listing:read', 'cheese_listing:write'])]
     private ?int $price = null;
 
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
 
     #[ORM\Column]
-    private ?bool $isPublished = null;
+    private ?bool $isPublished = false;
 
 
     public function __construct()
@@ -66,6 +82,17 @@ class CheeseListing
 
         return $this;
     }
+    
+    /**
+     * The description of the cheese as raw text.
+     */
+    #[Groups(['cheese_listing:write'])]
+    public function setTextDescription(string $description): self
+    {
+        $this->description = nl2br($description);
+
+        return $this;
+    }
 
     public function getPrice(): ?int
     {
@@ -78,12 +105,15 @@ class CheeseListing
 
         return $this;
     }
-
     public function getCreatedAt(): ?\DateTimeImmutable
     {
         return $this->createdAt;
     }
     
+    /**
+     * How long ago in text that this cheese listing was added.
+     */
+    #[Groups(['cheese_listing:read'])]
     public function getCreatedAtAgo(): string
     {
         return Carbon::instance($this->getCreatedAt())->diffForHumans();
